78a9f264854dd55ec7e2ce44805d3a9ce63f3f0d
Joshua Golafshan <JGolafshan@outlook.com>
2025-04-06 15:49:43 +1000

    monte carlo simulation class generated

---
+++ src/core/monte_carlo_simulation.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+    Date: 06/04/2024
+    Author: Joshua David Golafshan
+"""
+
+
+class MonteCarloSimulation:
+    def __init__(self):
+        pass
