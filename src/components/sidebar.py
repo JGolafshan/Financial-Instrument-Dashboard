a29909b0fb69999be175e901909a798745867f59
Joshua Golafshan <JGolafshan@outlook.com>
2025-04-07 01:23:12 +1000

    refactored sidebar

--- src/components/sidebar.py
+++ src/components/sidebar.py
@@ -7,28 +7,44 @@
 """
 
 import streamlit as st
-from src.utils import utils
+from src.utils.utils import search_logic
 
 
 def sidebar():
     st.sidebar.title("📊 Stock Dashboard")
 
-    # Search for a stock
-    search_query = st.sidebar.text_input("Search For a Ticker/Symbol:", "", placeholder="AAPL", key="small_search")
-    utils.search_logic(search_query)
+    # Search bar container
+    with st.sidebar.container(key="search_bar_container"):
+        search_columns = st.sidebar.columns([0.9, 0.1])
+        with search_columns[0]:
+            search_query = st.sidebar.text_input("Search For a Ticker/Symbol:", "", placeholder="AAPL",
+                                                 key="small_search")
 
-    # Show the navigation
+        with search_columns[1]:
+            if st.button("🔍", use_container_width=True):
+                if search_query:
+                    search_logic(search_query)
+
+    add_navigation_links()
+    author_details()
+
+
+def add_navigation_links():
+    # Add navigation links to the sidebar
     st.sidebar.page_link("pages/home.py", label="Home", icon=":material/home_filled:")
     st.sidebar.page_link("pages/instrument.py", label="Instrument Dashboard", icon=":material/dashboard:")
     # st.sidebar.page_link("pages/instruments.py", label="Instruments", icon=":material/explore:")
     st.sidebar.page_link("pages/queries.py", label="Queries", icon=":material/history:")
 
+
+def author_details():
     # Creator container w/ linked-in link
+    linked_in_link = "https://au.linkedin.com/in/jgd000"
     st.sidebar.markdown(
-        """
+        f"""
         <div id="creator-container">
             <p><code class="code_block">Created by:</code></p>
-            <a href="https://au.linkedin.com/in/jgd000" target="_blank">
+            <a href="{linked_in_link}" target="_blank">
                 <img src="https://cdn-icons-png.flaticon.com/512/174/174857.png" width="25" height="25">
                 <code class="code_block" style="display:inline;">Joshua David Golafshan</code>
             </a>
