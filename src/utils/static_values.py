043eea219a7ffc67de5c8818bcb85ba4463b00fe
Joshua Golafshan <JGolafshan@outlook.com>
2025-04-10 02:14:11 +1000

    queries.py complete, all filters added

---
+++ src/utils/static_values.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+    Date: 08/04/2024
+    Author: Joshua David Golafshan
+"""
+
+static_page_names = ["Select a Page URL", 'pages/queries.py', 'pages/instruments.py', 'pages/search.py', 'pages/instrument.py', 'pages/home.py']
+static_page_types = ["Select a Page Status", "viewed", "searched"]
