d5b8c74d59a6398950361ea74c8b667ea6608b55
Joshua Golafshan <JGolafshan@outlook.com>
2025-04-10 22:02:57 +1000

    custom metric for bs model UI added

---
+++ src/components/custom_metric.py
@@ -0,0 +1,23 @@
+def option_metric(css_style: str, option_type: str, option_price: float, option_delta: float, option_theta: float, option_rho: float):
+    return f"""
+        <div class="metric-container">
+            <div class="{css_style}">
+                <div class="metric-item">
+                    <div class="metric-label">{option_type}</div>
+                    <div class="metric-value">${option_price:.2f}</div>
+                </div>
+                <div class="metric-item">
+                    <div class="metric-label">Delta</div>
+                    <div class="metric-value">{option_delta:.2f}</div>
+                </div>
+                <div class="metric-item">
+                    <div class="metric-label">Theta</div>
+                    <div class="metric-value">{option_theta:.2f}</div>
+                </div>
+                <div class="metric-item">
+                    <div class="metric-label">Rho</div>
+                    <div class="metric-value">{option_rho:.2f}</div>
+                </div>
+            </div>
+        </div>
+    """
