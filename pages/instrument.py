565bf368f998467e566ac33ac6c7cbb75878d8bc
Joshua Golafshan <JGolafshan@outlook.com>
2025-04-10 22:26:02 +1000

    instrument.py UI improved, not complete.

--- pages/instrument.py
+++ pages/instrument.py
@@ -6,53 +6,72 @@
     Author: Joshua David Golafshan
     Description: all individual stock (ticker relate functions)
 """
-
+import pprint
+import time
+import orjson
+import datetime
 import numpy as np
 import streamlit as st
-from src.utils import utils
+from zoneinfo import ZoneInfo
 import plotly.graph_objs as go
-from src.utils.utils import set_page_state
+from src.utils.utils import set_page_state, yahoo_data
+from streamlit_javascript import st_javascript
+from src.components.custom_metric import option_metric
 from src.components.heatmap_graph import plot_heatmap
 from src.core.black_scholes_model import BlackScholesModel
+from src.core.monte_carlo_simulation import MonteCarloSimulation
 
 # Load Components
 set_page_state("pages/instrument.py")
-
 instrument_code = st.session_state.get("code", "NONE")
 st.query_params.code = instrument_code
 
-print(st.query_params.to_dict())
-print(st.session_state.to_dict())
+
+@st.cache_data(show_spinner="Fetching instrument data...")
+def get_instrument_data(symbol: str):
+    ticker = yahoo_data(symbol)
+    return {
+        "info": ticker.info,
+        "history": ticker.history(period="max")
+    }
 
 
-def calculate_price_difference(stock_data):
-    latest_price = stock_data.iloc[-1]["Close"]
-    previous_year_price = stock_data.iloc[-252]["Close"] if len(stock_data) > 252 else stock_data.iloc[0]["Close"]
-    price_difference = latest_price - previous_year_price
-    percentage_difference = (price_difference / previous_year_price) * 100
-    return price_difference, percentage_difference
+@st.cache_data(show_spinner="Loading market data...")
+def load_market_data(symbol: str):
+    """Load and clean market data from JSON file."""
+    with open("static/market_data.json") as f:
+        data = orjson.loads(f.read())
+        return data[symbol]
 
 
-def display_instrument_metrics(stock_data):
-    if stock_data is not None:
-        price_difference, percentage_difference = calculate_price_difference(stock_data)
-        latest_close_price = stock_data.iloc[-1]["Close"]
-        max_52_week_high = stock_data["High"].tail(252).max()
-        min_52_week_low = stock_data["Low"].tail(252).min()
+def calculate_user_time():
+    timezone = st_javascript("""await (async () => {
+                const userTimezone = Intl.DateTimeFormat().resolvedOptions().timeZone;
+                console.log(userTimezone)
+                return userTimezone
+    })().then(returnValue => returnValue)""", "casdsdslulate_user_time")
+    user_timedate = datetime.datetime.now(tz=ZoneInfo(timezone))
+    return user_timedate
 
-    col0, col1, col2, col3, col4, col5 = st.columns([1, 1, 1, 1, 1, 1])
-    with col0:
-        st.metric("Market Open/Closed", f"{'TIme till Market Opens/CLoses'}")
-    with col1:
-        st.metric("Market Open/Closed", f"{'TIme till Market Opens/CLoses'}")
-    with col2:
-        st.metric("Close Price", f"${latest_close_price:.2f}")
-    with col3:
-        st.metric("Price Difference (YoY)", f"${price_difference:.2f}", f"{percentage_difference:+.2f}%")
-    with col4:
-        st.metric("52-Week High", f"${max_52_week_high:.2f}")
-    with col5:
-        st.metric("52-Week Low", f"${min_52_week_low:.2f}")
+
+def calculate_market_time(exchange_data: dict):
+    pass
+
+
+def market__status():
+    pass
+
+
+def market_status(is_market_open: bool):
+    if is_market_open:
+        return st.metric("Market closes in", f"{'~6 Hours'}")
+    return st.metric("Market opens in ", f"{'~12 Hours'}")
+
+
+def instrument_change(is_market_open: bool):
+    if is_market_open:
+        return st.metric("Current Price", f"${178.42}", f"{1.03:+.2f}%")
+    return st.metric("Previous Day Change", f"${178.42}", f"{1.03:+.2f}%")
 
 
 def display_instrument(stock_info):
@@ -64,83 +83,85 @@ def display_instrument(stock_info):
             """)
 
 
-def show_bs_model():
-    col1, col2, col3 = st.columns(3, gap="medium")
+def calculate_stock_summary_statistics(stock_data):
+    latest_price = stock_data.iloc[-1]["Close"]
+    previous_year_price = stock_data.iloc[-252]["Close"] if len(stock_data) > 252 else stock_data.iloc[0]["Close"]
+    price_diff = latest_price - previous_year_price
+    pct_diff = (price_diff / previous_year_price) * 100
+    latest_close_price = stock_data.iloc[-1]["Close"]
+    high_52w = stock_data["High"].tail(252).max()
+    low_52w = stock_data["Low"].tail(252).min()
 
+    return price_diff, pct_diff, latest_close_price, high_52w, low_52w
+
+
+def display_summary_statistics(stock_data):
+    latest_close_price, prc_diff, pct_diff, high_52_w, low_52_w = calculate_stock_summary_statistics(stock_data)
+    col0, col1, col2, col3, col4, col5 = st.columns([1, 1, 1, 1, 1, 1])
+
+    with col0:
+        instrument_change(False)
     with col1:
-        # You can add inputs or explanations here
+        market_status(True)
+    with col2:
+        st.metric("Close Price", f"${latest_close_price:.2f}")
+    with col3:
+        st.metric("Price Difference (YoY)", f"${prc_diff:.2f}", f"{pct_diff:+.2f}%")
+    with col4:
+        st.metric("52-Week High", f"${high_52_w:.2f}")
+    with col5:
+        st.metric("52-Week Low", f"${low_52_w:.2f}")
+
+
+def show_bs_model():
+    col1, col2 = st.columns(2, gap="medium")
+    with col1:
         st.markdown("#### Option Pricing Parameters")
-        input_columns = st.columns(2)
+        input_column_1, input_column_2 = st.columns(2)
 
-        with input_columns[0]:
-            current_price = st.number_input(label="Current Price",
-                                            value=st.session_state.get("bs_current_price", 40),
-                                            key="bs_current_price")
-
-            strike = st.number_input(label="Strike", value=45, key="bs_strike")
-
-        with input_columns[1]:
-            volatility = st.number_input(label="Volatility", value=0.2, key="bs_volatility")
-            interest_rate = st.number_input(label="Interest rate", value=0.05, key="bs_interest_rate")
-
+        current_price = input_column_1.number_input(label="Current Price",
+                                                    value=st.session_state.get("bs_current_price", 40),
+                                                    key="bs_current_price")
+        strike = input_column_1.number_input(label="Strike", value=45, key="bs_strike")
+        volatility = input_column_2.number_input(label="Volatility", value=0.2, key="bs_volatility")
+        interest_rate = input_column_2.number_input(label="Interest rate", value=0.05, key="bs_interest_rate")
         time_to_maturity = st.number_input(label="Time to maturity", value=1, key="bs_time_to_maturity")
 
-    # Compute Call and Put values
     bs_model = BlackScholesModel(time_to_maturity, strike, current_price, volatility, interest_rate)
     call_price, put_price = bs_model.calculate_prices()
+    greeks = bs_model.calculate_greeks()
 
     with col2:
-        st.markdown("#### Computed Prices")
-        st.markdown(f"""
-                    <div class="metric-container metric-call">
-                        <div>
-                            <div class="metric-label">CALL Value</div>
-                            <div class="metric-value">${call_price:.2f}</div>
-                        </div>
-                    </div>
-                """, unsafe_allow_html=True)
-
-        st.markdown(f"""
-                    <div class="metric-container metric-put">
-                        <div>
-                            <div class="metric-label">PUT Value</div>
-                            <div class="metric-value">${put_price:.2f}</div>
-                        </div>
-                    </div>
-                """, unsafe_allow_html=True)
-
-    with col3:
-        bs_model = BlackScholesModel(time_to_maturity, strike, current_price, volatility, interest_rate)
-        greeks = bs_model.calculate_greeks()
-        st.write(greeks)
+        st.markdown(f"#### Computed Prices: Gamma={greeks["gamma"]:.2f}, Vega={greeks["vega"]:.2f}")
+        st.html(option_metric(css_style="metric-call", option_type="Call Value", option_price=call_price,
+                              option_delta=greeks["call_delta"], option_theta=greeks["call_theta"],
+                              option_rho=greeks["call_rho"]))
+        st.html(option_metric(css_style="metric-put", option_type="Put Value", option_price=put_price,
+                              option_delta=greeks["put_delta"], option_theta=greeks["put_theta"],
+                              option_rho=greeks["put_rho"]))
 
     st.markdown("---")
     st.subheader("🎯 Options Price - Interactive Heatmap")
 
-    with st.expander("⚙️ Heatmap Settings", expanded=True):
+    with st.expander("⚙️ Heatmap Settings", expanded=False):
         c1, c2 = st.columns(2)
-        with c1:
-            vol_min = st.slider('Min Volatility', 0.01, 1.0, value=volatility * 0.5, step=0.01)
-            spot_min = st.number_input('Min Spot Price', 0.01, value=current_price * 0.8, step=0.01)
-        with c2:
-            vol_max = st.slider('Max Volatility', 0.01, 1.0, value=volatility * 1.5, step=0.01)
-            spot_max = st.number_input('Max Spot Price', 0.01, value=current_price * 1.2, step=0.01)
+        vol_min = c1.slider('Min Volatility', 0.01, 1.0, value=volatility * 0.5, step=0.01)
+        vol_max = c1.slider('Max Volatility', 0.01, 1.0, value=volatility * 1.5, step=0.01)
+        spot_min = c2.number_input('Min Spot Price', 0.01, value=current_price * 0.8, step=0.01)
+        spot_max = c2.number_input('Max Spot Price', 0.01, value=current_price * 1.2, step=0.01)
 
+        # Generate this in function
         spot_range = np.linspace(spot_min, spot_max, 10)
         vol_range = np.linspace(vol_min, vol_max, 10)
 
-    heat_col1, heat_col2 = st.columns(2, gap="medium")
-
     fig_call, fig_put = plot_heatmap(bs_model, spot_range, vol_range, strike)
-
-    with heat_col1:
-        st.plotly_chart(fig_call, use_container_width=True, config={'displayModeBar': False})
-
-    with heat_col2:
-        st.plotly_chart(fig_put, use_container_width=True, config={'displayModeBar': False})
+    heat_col1, heat_col2 = st.columns(2, gap="medium")
+    heat_col1.plotly_chart(fig_call, use_container_width=True, config={'displayModeBar': False})
+    heat_col2.plotly_chart(fig_put, use_container_width=True, config={'displayModeBar': False})
 
 
 def plot_historical_chart(stock_data):
+    # TODO: Make this into .py
     candlestick_chart = go.Figure(data=[
         go.Candlestick(x=stock_data.index, open=stock_data['Open'], high=stock_data['High'],
                        low=stock_data['Low'], close=stock_data['Close'])])
@@ -160,35 +181,48 @@ def plot_historical_chart(stock_data):
     st.plotly_chart(candlestick_chart, use_container_width=True)
 
 
+def show_monte_carlo_page():
+    st.subheader("📈 Monte Carlo Simulation ")
+    with st.expander("⚙️ Settings", expanded=False):
+        number_of_simulations = st.number_input('Number of Simulations', min_value=1, value=30, step=5)
+        look_back = st.number_input('Look Back Period', min_value=1, value=30, step=5)
+        look_forward = st.number_input('Look Forward Period', 1, value=30, step=5)
+
+    mc_sim = MonteCarloSimulation(get_instrument_data(instrument_code)["history"], look_forward, number_of_simulations)
+    mc_sim.simulate()
+    st.line_chart(mc_sim.get_simulation_results())
+
+
 def show_info():
-    instrument_yahoo = utils.yahoo_data(instrument_code)
+    instrument_data = get_instrument_data(instrument_code)
+    if instrument_data is None:
+        st.warning(f"Instrument {instrument_code} not found.")
+        return
 
-    if instrument_yahoo:
-        stock_data = instrument_yahoo.history()
-        stock_info = instrument_yahoo.info
+    stock_info = instrument_data["info"]
+    pprint.pprint(stock_info)
+    stock_data = instrument_data["history"]
 
-        display_instrument(stock_info)
-        display_instrument_metrics(stock_data)
+    display_instrument(stock_info)
+    display_summary_statistics(stock_data)
 
-    hist_chart, bs_model, monete_carlo_sim = st.tabs(["📈 Historical Chart", "🧮 Black-Scholes", "🎲 Monte Carlo"])
-    with hist_chart:
+    # Use distinct tab names
+    chart_tab, bs_model_tab, monte_carlo_tab = st.tabs(["📈 Historical Chart", "🧮 Black-Scholes", "🎲 Monte Carlo"])
+
+    with chart_tab:
         if not stock_data.empty:
             plot_historical_chart(stock_data)
 
-    with bs_model:
+    with bs_model_tab:
         show_bs_model()
 
-    with monete_carlo_sim:
-        if not stock_data.empty:
-            st.subheader("📈 Historicalsss Stock Price")
-
-            # User can pick what to view
-
-            st.line_chart(stock_data[["Close"]])
+    with monte_carlo_tab:
+        show_monte_carlo_page()
 
 
-# PAGE FALLBACK
 if instrument_code and instrument_code != "NONE":
     show_info()
 else:
+    st.warning(f"Instrument {instrument_code} not found. Redirecting...")
+    time.sleep(2)
     st.switch_page("pages/search.py")
--- src/core/monte_carlo_simulation.py
+++ src/core/monte_carlo_simulation.py
@@ -1,12 +1,62 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-
-"""
-    Date: 06/04/2024
-    Author: Joshua David Golafshan
-"""
+import numpy as np
+import pandas as pd
 
 
 class MonteCarloSimulation:
-    def __init__(self):
-        pass
+    def __init__(self, data, forward_period, num_simulations):
+        """
+        Initializes the Monte Carlo Simulation.
+
+        :param data: Historical data in the form of a pandas DataFrame.
+        :param forward_period: Number of periods to simulate into the future.
+        :param num_simulations: Number of simulations to run.
+        """
+        self.data = data
+        self.forward_period = forward_period
+        self.num_simulations = num_simulations
+        self.simulation_results = None
+
+    def simulate(self):
+        """
+        Runs the Monte Carlo simulation to generate future outcomes.
+        """
+        # Initialize a DataFrame to store simulation results
+        simulation_outcomes = np.zeros((self.num_simulations, self.forward_period))
+
+        # Generate random outcomes based on the historical data
+        for i in range(self.num_simulations):
+            # Sample from the historical data and simulate forward
+            random_walk = np.random.choice(self.data.values.flatten(), size=self.forward_period)
+            simulation_outcomes[i, :] = random_walk
+
+        # Convert the results to a DataFrame
+        self.simulation_results = pd.DataFrame(simulation_outcomes,
+                                               columns=[f'Simulation{i + 1}' for i in range(self.forward_period)])
+
+    def get_simulation_results(self):
+        """
+        Returns the results of the simulation as a DataFrame.
+        """
+        if self.simulation_results is None:
+            raise ValueError("Simulation has not been run yet. Call simulate() first.")
+        return self.simulation_results
+
+    def get_mean_outcome(self):
+        """
+        Returns the mean outcome across all simulations for each period as a DataFrame.
+        """
+        if self.simulation_results is None:
+            raise ValueError("Simulation has not been run yet. Call simulate() first.")
+        mean_outcome = self.simulation_results.mean(axis=0)
+        return mean_outcome
+
+    def get_percentile_outcome(self, percentile=50):
+        """
+        Returns the percentile outcome (default 50th percentile) across all simulations for each period as a DataFrame.
+
+        :param percentile: The percentile to return (default is 50th percentile, i.e., median).
+        """
+        if self.simulation_results is None:
+            raise ValueError("Simulation has not been run yet. Call simulate() first.")
+        percentile_outcome = self.simulation_results.quantile(q=percentile / 100, axis=0)
+        return percentile_outcome
