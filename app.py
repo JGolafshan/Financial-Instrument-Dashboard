00b3d9d80eaccd67ff0cfbc920ce8e0c06648ec3
Joshua Golafshan <JGolafshan@outlook.com>
2025-04-11 02:54:45 +1000

    improved caching

--- app.py
+++ app.py
@@ -14,22 +14,15 @@ from src.components import sidebar
 import extra_streamlit_components as stx
 from pymongo.server_api import ServerApi
 
-
 with st.empty():
     @st.cache_resource
-    def get_manager():
+    def get_cookie_manager():
         return stx.CookieManager()
 
 
-    cookie_manager = get_manager()
-
-    # Retrieve the existing user_id from cookies
-    user_id = cookie_manager.get("user_id")
-
-    # If no user_id exists, generate and store one
-    if user_id is None:
-        user_id = str(uuid.uuid4())
-        cookie_manager.set("user_id", user_id)
+    cookie_manager = get_cookie_manager()
+    user_id = cookie_manager.get("user_id") or str(uuid.uuid4())
+    cookie_manager.set("user_id", user_id)
     st.session_state["user_id"] = user_id
 
 
@@ -37,29 +30,30 @@ with st.empty():
 def get_db_connection():
     mong_data = st.secrets['mongo']
     uri = f"mongodb+srv://{mong_data['username']}:{mong_data['password']}@{mong_data['db_name']}/?appName={mong_data['appName']}"
-    client = pymongo.MongoClient(uri, server_api=ServerApi('1'))
-    return client
+    return pymongo.MongoClient(uri, server_api=ServerApi('1'))
 
 
-if "db_client" not in st.session_state:
-    st.session_state["db_client"] = get_db_connection()
+@st.cache_resource
+def get_pages():
+    return [
+        st.Page("pages/home.py", title="Landing Page", icon=":material/show_chart:", url_path="/home", default=True),
+        st.Page("pages/instrument.py", title="Instrument", icon=":material/show_chart:", url_path="/instrument"),
+        st.Page("pages/instruments.py", title="Instruments", icon=":material/show_chart:", url_path="/instruments"),
+        st.Page("pages/queries.py", title="Historical Queries", icon=":material/show_chart:", url_path="/history"),
+        st.Page("pages/search.py", title="Instrument Query", icon=":material/show_chart:", url_path="/search"),
+    ]
 
+
+st.session_state["db_client"] = get_db_connection()
 st.markdown(utils.load_css("assets/css/styles.css"), unsafe_allow_html=True)
 utils.user_component()
 
-pages = [
-    st.Page("pages/home.py", title="Landing Page", icon=":material/show_chart:"),
-    st.Page("pages/instrument.py", title="Instrument", icon=":material/show_chart:", url_path="/instrument"),
-    st.Page("pages/instruments.py", title="Instruments", icon=":material/show_chart:", url_path="/instruments"),
-    st.Page("pages/queries.py", title="Historical Queries", icon=":material/show_chart:", url_path="/history"),
-    st.Page("pages/search.py", title="Instrument Query", icon=":material/show_chart:", url_path="/search"),
-]
-
-pg = st.navigation(pages, expanded=True)
+pg = st.navigation(get_pages(), expanded=True)
 sidebar.sidebar()
 
 try:
     pg.run()
+
 except Exception as e:
     st.header("Error")
     st.error(f"An unexpected error occurred. Redirecting to error page... \n\n {e.__str__()}")
